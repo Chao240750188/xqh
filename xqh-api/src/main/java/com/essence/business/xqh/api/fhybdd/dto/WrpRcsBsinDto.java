f1a3a988f82b7ae46d5cb90a3ff3e50ccb88f9d5
NoBugNoCode <2921345889@qq.com>
2021-01-25 09:36:36 +0800

    编写解析入库

---
+++ xqh-api/src/main/java/com/essence/business/xqh/api/fhybdd/dto/WrpRcsBsinDto.java
@@ -0,0 +1,143 @@
+package com.essence.business.xqh.api.fhybdd.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class WrpRcsBsinDto implements Serializable {
+    private String rvcrcrsccd;
+    private String rvcrcrscnm;
+    private String mnun;
+    private String cmun;
+    private String addvcd;
+    private String vltw;
+    private String ctcd;
+    private Long lgtd;
+    private Long lttd;
+    private Long crscwd;
+    private Long rvchwd;
+    private Long rvmnchel;
+    private String dtpl;
+    private String rvcrcrscov;
+    private Date dtupdt;
+
+
+    public String getRvcrcrsccd() {
+        return rvcrcrsccd;
+    }
+
+    public void setRvcrcrsccd(String rvcrcrsccd) {
+        this.rvcrcrsccd = rvcrcrsccd;
+    }
+
+    public String getRvcrcrscnm() {
+        return rvcrcrscnm;
+    }
+
+    public void setRvcrcrscnm(String rvcrcrscnm) {
+        this.rvcrcrscnm = rvcrcrscnm;
+    }
+
+    public String getMnun() {
+        return mnun;
+    }
+
+    public void setMnun(String mnun) {
+        this.mnun = mnun;
+    }
+
+    public String getCmun() {
+        return cmun;
+    }
+
+    public void setCmun(String cmun) {
+        this.cmun = cmun;
+    }
+
+    public String getAddvcd() {
+        return addvcd;
+    }
+
+    public void setAddvcd(String addvcd) {
+        this.addvcd = addvcd;
+    }
+
+    public String getVltw() {
+        return vltw;
+    }
+
+    public void setVltw(String vltw) {
+        this.vltw = vltw;
+    }
+
+    public String getCtcd() {
+        return ctcd;
+    }
+
+    public void setCtcd(String ctcd) {
+        this.ctcd = ctcd;
+    }
+
+    public Long getLgtd() {
+        return lgtd;
+    }
+
+    public void setLgtd(Long lgtd) {
+        this.lgtd = lgtd;
+    }
+
+    public Long getLttd() {
+        return lttd;
+    }
+
+    public void setLttd(Long lttd) {
+        this.lttd = lttd;
+    }
+
+    public Long getCrscwd() {
+        return crscwd;
+    }
+
+    public void setCrscwd(Long crscwd) {
+        this.crscwd = crscwd;
+    }
+
+    public Long getRvchwd() {
+        return rvchwd;
+    }
+
+    public void setRvchwd(Long rvchwd) {
+        this.rvchwd = rvchwd;
+    }
+
+    public Long getRvmnchel() {
+        return rvmnchel;
+    }
+
+    public void setRvmnchel(Long rvmnchel) {
+        this.rvmnchel = rvmnchel;
+    }
+
+    public String getDtpl() {
+        return dtpl;
+    }
+
+    public void setDtpl(String dtpl) {
+        this.dtpl = dtpl;
+    }
+
+    public String getRvcrcrscov() {
+        return rvcrcrscov;
+    }
+
+    public void setRvcrcrscov(String rvcrcrscov) {
+        this.rvcrcrscov = rvcrcrscov;
+    }
+
+    public Date getDtupdt() {
+        return dtupdt;
+    }
+
+    public void setDtupdt(Date dtupdt) {
+        this.dtupdt = dtupdt;
+    }
+}
