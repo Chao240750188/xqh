5e8c6055fd46daac23361978ce057d581d035582
NoBugNoCode <2921345889@qq.com>
2021-01-27 09:32:56 +0800

    水文预报模型对接第二版本调整

--- xqh-api/src/main/java/com/essence/business/xqh/api/fhybdd/service/ModelCallFhybdd2Service.java
+++ xqh-api/src/main/java/com/essence/business/xqh/api/fhybdd/service/ModelCallFhybdd2Service.java
@@ -1,27 +1,32 @@
 package com.essence.business.xqh.api.fhybdd.service;
 
-<<<<<<< HEAD
-import com.essence.business.xqh.api.fhybdd.dto.ModelCallBySWDDVo;
-import com.essence.business.xqh.api.fhybdd.dto.WrpRcsBsinDto;
-import com.essence.business.xqh.api.fhybdd.dto.WrpRvrBsinDto;
-import com.essence.business.xqh.api.fhybdd.dto.YwkModelDto;
-
-import java.util.List;
-import java.util.Map;
-
-=======
->>>>>>> 5ab077d7eda59d964a165310aff6cc3eb320276d
 public interface ModelCallFhybdd2Service {
 
     /**
      * 获取某段时间的降雨量，然后调用水文调度模型
+     *
      * @param planId
      */
-<<<<<<< HEAD
-    Object callMode(String planId);
-=======
     Long callMode(String planId);
->>>>>>> 5ab077d7eda59d964a165310aff6cc3eb320276d
 
 
+    /**
+     * 查询模型运行状态是否运行结束
+     * @return
+     */
+    String getModelRunStatus(String planId);
+
+    /**
+     * 查看输出结果
+     * @param planId
+     * @return
+     */
+    Object getModelResultQ(String planId);
+
+    /**
+     * 方案计算相关数据入库
+     * @param planId
+     * @return
+     */
+    Object saveModelData(String planId);
 }
