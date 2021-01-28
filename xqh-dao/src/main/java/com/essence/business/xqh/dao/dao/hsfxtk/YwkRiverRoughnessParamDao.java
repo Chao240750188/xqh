698fdc4480e2a7055f6bc3900215911c06f842df
xiongchao <240750188@qq.com>
2021-01-28 20:35:16 +0800

    解决冲突

--- xqh-dao/src/main/java/com/essence/business/xqh/dao/dao/hsfxtk/YwkRiverRoughnessParamDao.java
+++ xqh-dao/src/main/java/com/essence/business/xqh/dao/dao/hsfxtk/YwkRiverRoughnessParamDao.java
@@ -9,14 +9,12 @@ import java.util.List;
 
 @Repository
 public interface YwkRiverRoughnessParamDao extends EssenceJpaRepository<YwkRiverRoughnessParam, String> {
-<<<<<<< HEAD
     /**
      * 根据模型糙率参数查询河道糙率参数列表
      * @param roughnessParamid
      * @return
      */
     List<YwkRiverRoughnessParam> findByRoughnessParamid(String roughnessParamid);
-=======
 
 
     @Query(value = "select * from YWK_RIVER_ROUGHNESS_PARAM where ROUGHNESS_PARAMID =?1 order by MILEAGE asc ",nativeQuery = true)
@@ -24,5 +22,4 @@ public interface YwkRiverRoughnessParamDao extends EssenceJpaRepository<YwkRiver
 
 
     void deleteByRoughnessParamid(String RoughnessParamid);
->>>>>>> ce33305276b9abef21c305f50a1c6fd24b1689d5
 }
