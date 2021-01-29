4ba4fc9330d315e76c165e129848a62c71e28992
fengpanpan <814200405@qq.com>
2021-01-29 09:44:45 +0800

    note#水情

--- xqh-dao/src/main/java/com/essence/business/xqh/dao/dao/realtimemonitor/TRsvrfsrBDao.java
+++ xqh-dao/src/main/java/com/essence/business/xqh/dao/dao/realtimemonitor/TRsvrfsrBDao.java
@@ -2,6 +2,7 @@ package com.essence.business.xqh.dao.dao.realtimemonitor;
 
 import com.essence.business.xqh.dao.entity.realtimemonitor.TRsvrfsrB;
 import com.essence.framework.jpa.EssenceJpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public interface TRsvrfsrBDao extends EssenceJpaRepository<TRsvrfsrB, String> {
     TRsvrfsrB findByStcdAndFstp(String stcd, String fstp);
 
 
+    @Query(value="select t from TRsvrfsrB t where t.stcd in (?1) and t.fstp=?2")
     List<TRsvrfsrB> findByStcdInaAndFstp(List<String> stcdList,String fstp);
 
 
