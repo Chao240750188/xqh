d170c62575180c662e2337ecbae9a729aa6d3679
huangxiaoli <809722507@qq.com>
2021-02-03 11:35:31 +0800

    模型结果转为图片

---
+++ xqh-api/src/main/java/com/essence/business/xqh/api/modelResult/ModelResultService.java
@@ -0,0 +1,18 @@
+package com.essence.business.xqh.api.modelResult;
+
+import com.essence.business.xqh.api.modelResult.dto.GridResultDto;
+
+import java.util.List;
+
+public interface ModelResultService {
+
+    /**
+     * 将模型结果生成图片
+     * @Author huangxiaoli
+     * @Description
+     * @Date 15:53 2021/2/1
+     * @Param [gridResultDtoList, modelName]
+     * @return void
+     **/
+    public void getResultMaxDepthToPicture(List<GridResultDto> gridResultDtoList,String modelId,String planId,Object processNum);
+}
--- xqh-common/src/main/java/com/essence/business/xqh/common/util/gis/GisUtil.java
+++ xqh-common/src/main/java/com/essence/business/xqh/common/util/gis/GisUtil.java
@@ -1,160 +1,157 @@
 package com.essence.business.xqh.common.util.gis;
 
-import com.essence.business.xqh.common.util.word.AttachmentPathUtil;
+import com.essence.business.xqh.common.util.ExportMethodResultDto;
+import com.essence.business.xqh.common.util.GisPathConfigurationUtil;
+import net.sf.json.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GisUtil {
+
     /**
-     * 获取乡镇街道信息
+     * 将 shp文件 调用GIS服务 替换数据源  导出png图片
+     * @param inAbsolutePath    shp文件夹路径
+     * @param inFileName    shp文件名
+     * @param mxdTemplateAbsolutePath 渲染数据文件的文件夹路径
+     * @param mxdTemplate     渲染数据文件的文件名
+     * @param exportFormat   导出格式
+     * @param outputAbsolutePath  导出图片文件的文件夹路径
+     * @param outputPictureName  到处图片的文件名
      */
-    public  String getWaterSlice(String gisUrl0) throws Exception {
+    public static ExportMethodResultDto exportToPicture(String inAbsolutePath, String inFileName, String mxdTemplateAbsolutePath, String mxdTemplate, String exportFormat, String outputAbsolutePath, String outputPictureName){
 
-        // 创建默认的httpClient实例
+        ExportMethodResultDto resultInfo = new ExportMethodResultDto();
+
+        String jobId=null;
+        // 创建默认的httpClient实例.
         CloseableHttpClient httpclient = HttpClients.createDefault();
+
+
         // 创建httppost
-        HttpPost httppost = new HttpPost(gisUrl0);
+        HttpPost httppost = new HttpPost("http://"+ GisPathConfigurationUtil.getArcGISServerHostIP()+":6080/arcgis/rest/services/xqh/exportToPicture/GPServer/exportToPicture/submitJob");
+
+        // 创建参数队列
         // 创建参数队列
         List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("where", "1=1"));
-        params.add(new BasicNameValuePair("geometryType", "esriGeometryEnvelope"));
-        params.add(new BasicNameValuePair("spatialRel", "esriSpatialRelIntersects"));
-        params.add(new BasicNameValuePair("outFields", "*"));
-        params.add(new BasicNameValuePair("returnGeometry", "true"));
-        params.add(new BasicNameValuePair("returnIdsOnly", "false"));
-        params.add(new BasicNameValuePair("returnCountOnly", "false"));
-        params.add(new BasicNameValuePair("returnZ", "false"));
-        params.add(new BasicNameValuePair("returnM", "false"));
-        params.add(new BasicNameValuePair("returnDistinctValues", "false"));
-        params.add(new BasicNameValuePair("f", "pjson"));
-        HttpEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
-        httppost.setEntity(entity);
-        CloseableHttpResponse response = httpclient.execute(httppost);
-        HttpEntity responseEntity = response.getEntity();
-        String responseContent = EntityUtils.toString(responseEntity, "UTF-8");
+        params.add(new BasicNameValuePair("env:outSR", ""));
+        params.add(new BasicNameValuePair("env:processSR",""));
+        params.add(new BasicNameValuePair("f","pjson"));//format设置成json
+        params.add(new BasicNameValuePair("inAbsolutePath",inAbsolutePath));
+        System.out.println("inAbsolutePath:"+inAbsolutePath);
+        params.add(new BasicNameValuePair("inFileName",inFileName));//不加后缀
+        System.out.println("inFileName:"+inFileName);
+        params.add(new BasicNameValuePair("mxdTemplateAbsolutePath",mxdTemplateAbsolutePath));
+        System.out.println("mxdTemplateAbsolutePath:"+mxdTemplateAbsolutePath);
+        params.add(new BasicNameValuePair("mxdTemplate",mxdTemplate));//不加后缀
+        System.out.println("mxdTemplate:"+mxdTemplate);
+        params.add(new BasicNameValuePair("exportFormat",exportFormat));//格式为.png
+        System.out.println("exportFormat:"+exportFormat);
+        params.add(new BasicNameValuePair("outputAbsolutePath",outputAbsolutePath));
+        System.out.println("outputAbsolutePath:"+outputAbsolutePath);
+        params.add(new BasicNameValuePair("outputPictureName",outputPictureName));
+        System.out.println("outputPictureName:"+outputPictureName);
 
-        if (!responseContent.contains("features")) {
-            throw new RuntimeException("调用GIS服务-获取水闸，错误信息：" + responseContent);
+        params.add(new BasicNameValuePair("returnM","false"));
+        params.add(new BasicNameValuePair("returnZ","false"));
+        HttpEntity entity;
+        CloseableHttpResponse response =null;
+        try {
+            entity = new UrlEncodedFormEntity(params, "UTF-8");
+            httppost.setEntity(entity);
+            response = httpclient.execute(httppost);
+
+            HttpEntity responseEntity = response.getEntity();
+            if (responseEntity != null) {
+                String responseContent = EntityUtils.toString(responseEntity, "UTF-8");
+                if(responseContent.contains("esriJobSubmitted")){
+
+                    JSONObject json= JSONObject.fromObject(responseContent);
+                    jobId = json.getString("jobId"); //异步服务返回的jobId
+
+                }
+            }
+            if (null==jobId){
+                throw new RuntimeException("图片转换失败");
+            }
+            resultInfo.setJobId(jobId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        response.close();
-        // 关闭连接,释放资源
-        httpclient.close();
-        return responseContent;
+
+        return resultInfo;
     }
 
-
     /**
-     * 获取雨晴等值面
-     * Reclassification         0.01 2.5 1;2.5 8 2;8 16 3;16 20 4;20 9999 5
+     * 获取生成图片执行状态
+     * @param resultInfo
+     * @return
      */
-    public String getHdyqDzm(String gisUrl0, String Reclassification, String inputRainfall_shp)throws Exception {
-        // 创建默认的httpClient实例
+    public static String getExportToPictureStatus(ExportMethodResultDto resultInfo){
+
+        // 创建默认的httpClient实例.
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        // 创建httppost
-        HttpPost httppost = new HttpPost(gisUrl0);
-        // 创建参数队列
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("Reclassification", Reclassification));
-        params.add(new BasicNameValuePair("inputRainfall_shp", inputRainfall_shp));
-        params.add(new BasicNameValuePair("returnZ", "false"));
-        params.add(new BasicNameValuePair("returnM", "false"));
-        params.add(new BasicNameValuePair("env:processSR", ""));
-        params.add(new BasicNameValuePair("env:outSR", ""));
-        params.add(new BasicNameValuePair("f", "pjson"));
-        HttpEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
-        httppost.setEntity(entity);
-        CloseableHttpResponse response = httpclient.execute(httppost);
-        HttpEntity responseEntity = response.getEntity();
-        String responseContent = EntityUtils.toString(responseEntity, "UTF-8");
-        if (!responseContent.contains("features")) {
-            throw new RuntimeException("调用GIS服务-获取水闸，错误信息：" + responseContent);
+
+        // 创建httpget
+        HttpGet httpget = new HttpGet("http://"+ GisPathConfigurationUtil.getArcGISServerHostIP()+":6080/arcgis/rest/services/xqh/exportToPicture/GPServer/exportToPicture/jobs/"+resultInfo.getJobId());
+
+        HttpEntity responseEntity;
+        CloseableHttpResponse response =null;
+        try {
+
+            response = httpclient.execute(httpget);
+
+            responseEntity = response.getEntity();
+            if (responseEntity != null) {
+                String responseContent = EntityUtils.toString(responseEntity, "UTF-8");
+                if(responseContent.contains("esriJobSucceeded")) { //包含字符串表示风险图生成完毕
+                    return  "true";
+                }else if (responseContent.contains("Failed")){
+                    return "failed";
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }  finally {
+            // 关闭连接,释放资源
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        response.close();
-        // 关闭连接,释放资源
-        httpclient.close();
-        return responseContent;
-    }
 
-
-
- /**
-  * 获取雨情趋势数据
-  * @Author huangxiaoli
-  * @Description
-  * @Date 16:50 2020/9/7
-  * @Param [inputRainfall_shp]
-  * @return java.lang.String
-  **/
-    public static String getGis3ddzm(String inputRainfall_shp)throws Exception {
-        // 创建默认的httpClient实例
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        // 创建httppost
-        HttpPost httppost = new HttpPost(AttachmentPathUtil.getGis3ddzmUrl());
-        // 创建参数队列
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("input_rainfall", inputRainfall_shp));
-        params.add(new BasicNameValuePair("returnZ", "false"));
-        params.add(new BasicNameValuePair("returnM", "false"));
-        params.add(new BasicNameValuePair("env:processSR", ""));
-        params.add(new BasicNameValuePair("env:outSR", ""));
-        params.add(new BasicNameValuePair("f", "pjson"));
-        HttpEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
-        httppost.setEntity(entity);
-        CloseableHttpResponse response = httpclient.execute(httppost);
-        HttpEntity responseEntity = response.getEntity();
-        String responseContent = EntityUtils.toString(responseEntity, "UTF-8");
-        if (!responseContent.contains("features")) {
-            throw new RuntimeException("调用GIS服务-获取水闸，错误信息：" + responseContent);
-        }
-        response.close();
-        // 关闭连接,释放资源
-        httpclient.close();
-        return responseContent;
-    }
-
-
-    /**
-     * 获取雨情趋势数据
-     * @Author huangxiaoli
-     * @Description
-     * @Date 10:46 2020/10/23
-     * @Param [inputRainfall_shp]
-     * @return java.lang.String
-     **/
-    public static String getGisTrend3ddzm(String inputRainfall_shp)throws Exception {
-        // 创建默认的httpClient实例
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        // 创建httppost
-        HttpPost httppost = new HttpPost(AttachmentPathUtil.getGis3ddzmUrl());
-        // 创建参数队列
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("input_rainfall", inputRainfall_shp));
-        params.add(new BasicNameValuePair("returnZ", "false"));
-        params.add(new BasicNameValuePair("returnM", "false"));
-        params.add(new BasicNameValuePair("env:processSR", "4326"));
-        params.add(new BasicNameValuePair("env:outSR", "4326"));
-        params.add(new BasicNameValuePair("f", "pjson"));
-        HttpEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
-        httppost.setEntity(entity);
-        CloseableHttpResponse response = httpclient.execute(httppost);
-        HttpEntity responseEntity = response.getEntity();
-        String responseContent = EntityUtils.toString(responseEntity, "UTF-8");
-        if (!responseContent.contains("features")) {
-            throw new RuntimeException("调用GIS服务-获取水闸，错误信息：" + responseContent);
-        }
-        response.close();
-        // 关闭连接,释放资源
-        httpclient.close();
-        return responseContent;
+        return "false";
     }
 }
