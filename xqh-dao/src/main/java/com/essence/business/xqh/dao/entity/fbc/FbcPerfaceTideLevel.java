aac1ffe7a57d1f04645c47584a0599a5eff7668b
xiongchao <240750188@qq.com>
2021-07-02 11:55:42 +0800

    风暴潮模型对接

---
+++ xqh-dao/src/main/java/com/essence/business/xqh/dao/entity/fbc/FbcPerfaceTideLevel.java
@@ -0,0 +1,71 @@
+package com.essence.business.xqh.dao.entity.fbc;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * 风暴潮-前序潮位
+ */
+@Entity
+@Table(name = "FBC_PERFACE_TIDE_LEVEL", schema = "XQH", catalog = "")
+public class FbcPerfaceTideLevel {
+    @Id
+    @Column(name = "C_ID")
+    private String cId;
+    @Column(name = "N_PLANID")
+    private String nPlanid;
+    @Column(name = "TIDE_LEVEL")
+    private Double tideLevel;
+    @Column(name = "RELATIVE_TIME")
+    private Long relativeTime;
+    @Column(name = "ABSOLUTE_TIME")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date absoluteTime;
+
+
+    public String getcId() {
+        return cId;
+    }
+
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    public String getnPlanid() {
+        return nPlanid;
+    }
+
+    public void setnPlanid(String nPlanid) {
+        this.nPlanid = nPlanid;
+    }
+
+    public Double getTideLevel() {
+        return tideLevel;
+    }
+
+    public void setTideLevel(Double tideLevel) {
+        this.tideLevel = tideLevel;
+    }
+
+    public Long getRelativeTime() {
+        return relativeTime;
+    }
+
+    public void setRelativeTime(Long relativeTime) {
+        this.relativeTime = relativeTime;
+    }
+
+    public Date getAbsoluteTime() {
+        return absoluteTime;
+    }
+
+    public void setAbsoluteTime(Date absoluteTime) {
+        this.absoluteTime = absoluteTime;
+    }
+}
---
+++ xqh-dao/src/main/java/com/essence/business/xqh/dao/entity/fbc/FbcShortestDistance.java
@@ -0,0 +1,70 @@
+package com.essence.business.xqh.dao.entity.fbc;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * 风暴潮-最短距离
+ */
+@Entity
+@Table(name = "FBC_SHORTEST_DISTANCE", schema = "XQH", catalog = "")
+public class FbcShortestDistance {
+    @Id
+    @Column(name = "C_ID")
+    private String cId;
+    @Column(name = "N_PLANID")
+    private String nPlanid;
+    @Column(name = "SHORTEST_DISTANCE")
+    private Double shortestDistance;
+    @Column(name = "RELATIVE_TIME")
+    private Long relativeTime;
+    @Column(name = "ABSOLUTE_TIME")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date absoluteTime;
+
+    public String getcId() {
+        return cId;
+    }
+
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    public String getnPlanid() {
+        return nPlanid;
+    }
+
+    public void setnPlanid(String nPlanid) {
+        this.nPlanid = nPlanid;
+    }
+
+    public Double getShortestDistance() {
+        return shortestDistance;
+    }
+
+    public void setShortestDistance(Double shortestDistance) {
+        this.shortestDistance = shortestDistance;
+    }
+
+    public Long getRelativeTime() {
+        return relativeTime;
+    }
+
+    public void setRelativeTime(Long relativeTime) {
+        this.relativeTime = relativeTime;
+    }
+
+    public Date getAbsoluteTime() {
+        return absoluteTime;
+    }
+
+    public void setAbsoluteTime(Date absoluteTime) {
+        this.absoluteTime = absoluteTime;
+    }
+}
