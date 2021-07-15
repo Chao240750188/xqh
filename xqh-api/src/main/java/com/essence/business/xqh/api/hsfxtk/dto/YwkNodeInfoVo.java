6ca5268da849a29e9b8f0d77f4408165562ffa39
wuhao <610776242@qq.com>
2021-07-15 16:03:01 +0800

    提交YWKNODEINFO

---
+++ xqh-api/src/main/java/com/essence/business/xqh/api/hsfxtk/dto/YwkNodeInfoVo.java
@@ -0,0 +1,24 @@
+package com.essence.business.xqh.api.hsfxtk.dto;
+
+public class YwkNodeInfoVo {
+
+    private String nPlanid;
+
+    private Double mileage;
+
+    public String getnPlanid() {
+        return nPlanid;
+    }
+
+    public void setnPlanid(String nPlanid) {
+        this.nPlanid = nPlanid;
+    }
+
+    public Double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Double mileage) {
+        this.mileage = mileage;
+    }
+}
